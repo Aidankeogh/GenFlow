00387b818ba763ecbf6083626b572d190cb67d0d
Aidan Keogh <aidankeogh@gmail.com>
2023-04-16 11:31:00 -0700

    changes from meeting

--- hydra_tests.py
+++ hydra_tests.py
@@ -3,11 +3,16 @@ import lightning
 import sklearn
 import hydra
 from omegaconf import DictConfig, OmegaConf
+import importlib
+from sklearn.linear_model import LogisticRegression
 
-print(torch.__version__)
-print(lightning.__version__)
-print(sklearn.__version__)
+# print(torch.__version__)
+# print(lightning.__version__)
+# print(sklearn.__version__)
 
+# linear = importlib.import_module("sklearn.linear_model")
+# LR = getattr(linear, "LogisticRegression")
+# print(LR, LogisticRegression)
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def my_app(cfg: DictConfig) -> None:
