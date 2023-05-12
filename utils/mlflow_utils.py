2b1b2707c2a967fbf62f87ed50fccfbd1e2690af
Aidan Keogh <aidan@narrows-gpu-1.cm.cluster>
2023-05-12 13:45:43 -0700

    updating mlflow logging

--- utils/mlflow_utils.py
+++ utils/mlflow_utils.py
@@ -2,16 +2,19 @@ from omegaconf import DictConfig, ListConfig
 import mlflow
 
 def log_params_from_omegaconf_dict(params):
+    """ Helper function for omegaconf logging from 
+    https://medium.com/optuna/easy-hyperparameter-management-with-hydra-mlflow-and-optuna-783730700e7d """
     for param_name, element in params.items():
         _explore_recursive(param_name, element)
 
 def _explore_recursive(parent_name, element):
-    if isinstance(element, DictConfig): 
+    if isinstance(element, DictConfig):
         for k, v in element.items():
             if isinstance(v, DictConfig) or isinstance(v, ListConfig):
                 _explore_recursive(f'{parent_name}.{k}', v)
             else:
                 mlflow.log_param(f'{parent_name}.{k}', v)
+
     elif isinstance(element, ListConfig):
         for i, v in enumerate(element):
             mlflow.log_param(f'{parent_name}.{i}', v)
