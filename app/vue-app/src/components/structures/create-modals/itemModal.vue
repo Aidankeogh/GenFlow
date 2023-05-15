9faa6eb8794ed61b6caea32cf836f186f652d3e0
leoclo <ccloleonardo@gmail.com>
2023-05-14 20:06:27 -0700

    Implemented Dashboard with markdown

---
+++ app/vue-app/src/components/structures/create-modals/itemModal.vue
@@ -0,0 +1,105 @@
+<template>
+    <ft-modal @close="$emit('close')" width="90%">
+        <template #header>
+            {{ modeTitle }}
+        </template>
+        <div class="ft-p-2">
+            <div class="ft-d-flex ft-mb-3">
+                <span class="ft-d-flex ft-flex-column ft-w-50">
+                    <label class="ft-mb-1" style="font-weight: bold;"> Component </label>
+                    <v-select 
+                        class="ft-w-50"
+                        v-model:modelValue="selVal"
+                        :options="compOptions"
+                        :clearable="false"
+                    ></v-select>
+                </span>
+            </div>
+            <ft-ace 
+                @init="initEditor"
+                v-model:value="markdownCode"
+                class="ft-ace-editor"
+                lang="markdown"
+                height="380px"
+                width="100%">
+            </ft-ace>
+            <div class="ft-d-flex ft-justify-content-center ft-mt-3">
+                <button @click="confirmChanges" class="ft-btn ft-border ft-btn-light ft-hover-text-white ft-hover-bg-ft ft-w-50">
+                    {{ modeTitle }}
+                </button>
+            </div>
+        </div>
+    </ft-modal>
+</template>
+
+<script>
+    import FtModal from '@/components/utils/ft-modal/FtModal.vue';
+    import FtAce from '@/components/utils/ft-ace/FtAce.vue';
+    import _ from 'lodash';
+
+    const compRef = {
+        'ft-markdown': 'Markdown'
+    }
+
+    export default {
+        name: 'item-modal',
+        components:{
+            FtModal,
+            FtAce
+        },
+        props:{
+            mode:{default: 'create'},
+            initialItem: {default: () => {return {}}}
+        },
+        computed:{
+            modeTitle(){
+                const titleRef = {
+                    'create': 'Add Component',
+                    'edit': 'Update Component'
+                }
+                return titleRef[this.mode]
+            }
+        },
+        data(){
+            return {
+                selVal: {
+                    'value': _.get(this.initialItem, 'comp.component', 'ft-markdown'),
+                    'label' : compRef[_.get(this.initialItem, 'comp.component', 'ft-markdown')],
+                }, 
+                markdownCode: _.get(this.initialItem, 'comp.bind.markdown', '### This is a Markdown'),
+                compOptions: Object.keys(compRef).map(key => {
+                    return {value: key, label: compRef[key]}
+                })
+            }
+        },
+        methods:{
+            initEditor(editor){
+                editor.renderer.setScrollMargin(3, 0);
+                editor.setOptions({
+                    printMargin: true,
+                    wrap: true,
+                    scrollPastEnd: 0.5,
+                });
+            },
+            confirmChanges(){
+                this.$emit(this.mode + 'Item', {
+                    ...this.initialItem, 
+                    comp: {
+                        ...this.getComp()
+                    }
+                })
+            },
+            getComp(){
+                switch (this.selVal.value){
+                    case 'ft-markdown':
+                        return {
+                            component: this.selVal.value,
+                            bind: {markdown: this.markdownCode}
+                        }
+                    default:
+                        return null;
+                }
+            }
+        }
+    }
+</script>
