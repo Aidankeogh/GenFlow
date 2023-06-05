96882fc6b5d548eebb7151e4feb35850c2cd1dc2
Aidan Keogh <aidan@narrows-gpu-1.cm.cluster>
2023-06-05 16:03:54 -0700

    added DR step, dataset saving, and new experiment files

--- training_pipeline.py
+++ training_pipeline.py
@@ -2,6 +2,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 from hydra.core.hydra_config import HydraConfig
 from dataset.data_builder import training_data
+from dataset.dimensionality_reduction import reduce_dimensionality
 from models.sklearn_factory import train_model
 from evaluator.r2_evaluator import evaluate_model
 from sklearn.model_selection import train_test_split
@@ -10,6 +11,8 @@ import numpy as np
 import hashlib
 import mlflow
 import warnings
+import os
+import pickle
 warnings.filterwarnings("ignore")
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
@@ -20,9 +23,30 @@ def my_app(cfg: DictConfig) -> None:
         print("Config: ")
         print(OmegaConf.to_yaml(cfg))
         log_params_from_omegaconf_dict(cfg)
-
-        X_geno, Y_pheno = training_data(**cfg.dataset.params).gwas_filtered(cfg.dataset.gwas_p_value)
-        X_train, X_val, y_train, y_val = train_test_split(X_geno, Y_pheno, test_size=0.20, random_state=42)
+        save_file = cfg.dataset.get("save_to_file", None)
+        
+        if save_file is not None and os.path.exists(save_file):
+            with open(save_file, 'rb') as f:
+                print(f"Loading saved dataset from {save_file}")
+                X_geno, Y_pheno = pickle.load(f)
+        else:
+            X_geno, Y_pheno = training_data(
+                    **cfg.dataset.params
+            ).gwas_filtered(cfg.dataset.gwas_p_value)
+            
+            if save_file is not None: 
+                with open(save_file, 'wb') as f:
+                    print(f"Saving dataset to {save_file}")
+                    pickle.dump( (X_geno, Y_pheno), f)
+                    
+                
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_geno, Y_pheno, test_size=0.20, random_state=42
+        )
+        
+        dr_cfg = cfg.get("dimensionality_reduction", None)
+        if dr_cfg is not None:
+            X_train, X_val = reduce_dimensionality(X_train, X_val, dr_cfg)            
 
         model = train_model(X_train, y_train, cfg.model)
         results = evaluate_model(model, X_val, y_val)
